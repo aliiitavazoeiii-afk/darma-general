a47c5961601205793235d6ead0c917e4ab1f03e1
aliiitavazoeiii-afk <aliiitavazoeiii@gmail.com>
2026-08-22 21:36:50 +0330

    Run final ERP state sync on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py seed_base
 python manage.py sync_sale_inventory
+python manage.py sync_financial_state
 python manage.py ensure_admin
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3 --threads 2 --timeout 60 --access-logfile - --error-logfile -
