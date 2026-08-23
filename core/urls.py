f308708f372d6759722dae8794752b9c5bb4bb30
aliiitavazoeiii-afk <aliiitavazoeiii@gmail.com>
2026-08-23 18:40:58 +0330

    Use brand-aware inventory view

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("material-report/<int:block_id>/delete/", excel_views.material_block_delete, name="material_block_delete"),
     path("takvin/", excel_takvin.takvin_excel, name="takvin"),
 
-    path("inventory/", final_views.inventory, name="inventory"),
+    path("inventory/", inventory_views.inventory, name="inventory"),
     path("inventory/color-model/add/", inventory_views.add_color_model, name="inventory_add_color_model"),
     path("inventory/operations/", final_views.inventory_operations, name="inventory_operations"),
 
