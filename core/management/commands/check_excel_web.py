9f65d03dfb9a130d92c0843011a040033f9c56ef
aliiitavazoeiii-afk <aliiitavazoeiii@gmail.com>
2026-08-25 18:04:52 +0330

    Extend preflight for material flow v13

--- core/management/commands/check_excel_web.py
+++ core/management/commands/check_excel_web.py
@@ -19,8 +19,8 @@ class Command(BaseCommand):
             "core/report_excel_v2.html", "core/report_excel_v3.html", "core/report_excel_v5.html", "core/report_excel_v9.html",
             "core/_manual_table.html", "core/_raw_fabric_table.html", "core/_raw_elastic_table.html",
             "core/_raw_material_panel_v3.html", "core/_financial_summary_extra.html",
-            "core/payments.html", "core/payments_v9.html", "core/calculator.html", "core/_calculator_result.html",
-            "core/material_report.html", "core/takvin_excel.html", "core/sale_calendar.html",
+            "core/payments.html", "core/payments_v9.html", "core/payments_v13.html", "core/calculator.html", "core/_calculator_result.html",
+            "core/material_report.html", "core/material_report_v13.html", "core/takvin_excel.html", "core/sale_calendar.html",
             "core/sale_brand_final.html", "core/sale_size.html", "core/_sale_saved_final.html",
             "core/daily_report.html", "core/daily_report_v8.html", "core/_daily_order_upload.html",
             "core/inventory_final.html", "core/inventory_operations.html",
@@ -53,7 +53,7 @@ class Command(BaseCommand):
         parameter_routes = [
             ("sale_brand", [1]), ("daily_report", [1]), ("daily_order_import", [1]),
             ("sale_size", [1, 1, 1]), ("shortage_resolve", [1]),
-            ("material_block_save", [1]), ("material_block_delete", [1]),
+            ("material_block_save", [1]), ("material_block_apply", [1]), ("material_block_unapply", [1]), ("material_block_delete", [1]),
             ("payment_delete", [1]), ("receipt_delete", [1]), ("settings_product_edit", [1]),
         ]
         for route, args in parameter_routes:
