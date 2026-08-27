6f3f62001a612ec00a0440405388c8e633ee8e8c
aliiitavazoeiii-afk <aliiitavazoeiii@gmail.com>
2026-08-27 18:10:24 +0330

    Use grouped alert delivery in Telegram bot

--- core/management/commands/run_inventory_bot.py
+++ core/management/commands/run_inventory_bot.py
@@ -2,7 +2,8 @@ import os
 
 from django.core.management.base import BaseCommand, CommandError
 
-from core.telegram_inventory_bot_v20 import InventoryBot, TelegramAPI, TelegramAPIError
+from core.telegram_inventory_alerts_v20 import BatchedInventoryBot
+from core.telegram_inventory_bot_v20 import TelegramAPI, TelegramAPIError
 
 
 class Command(BaseCommand):
@@ -13,6 +14,6 @@ class Command(BaseCommand):
         if not token:
             raise CommandError("TELEGRAM_BOT_TOKEN is not set")
         try:
-            InventoryBot(TelegramAPI(token)).run_forever()
+            BatchedInventoryBot(TelegramAPI(token)).run_forever()
         except TelegramAPIError as exc:
             raise CommandError(str(exc)) from exc
