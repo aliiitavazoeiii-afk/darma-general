ef84883d2af1f8571f6aab2ca44db03d69b155a7
aliiitavazoeiii-afk <aliiitavazoeiii@gmail.com>
2026-08-29 20:14:07 +0330

    wire isolated daily return route v36

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import business_tools_v14, business_tools_v21, business_tools_v22, calendar_views, catalog_v5, daily_order_views_v8, daily_report_actions_v21, daily_report_v8, daily_views, excel_dashboard, excel_sales, final_views, inventory_operations_v15, inventory_v20, material_report_v20, material_report_v21, material_report_v22, pricing_v7, report_v9, sale_brand_v19, settings_rules_v17, settings_stock_v5, takvin_v5, views
+from . import business_tools_v14, business_tools_v21, business_tools_v22, calendar_views, catalog_v5, daily_order_views_v8, daily_report_actions_v21, daily_report_v8, daily_returns_v36, daily_views, excel_dashboard, excel_sales, final_views, inventory_operations_v15, inventory_v20, material_report_v20, material_report_v21, material_report_v22, pricing_v7, report_v9, sale_brand_v19, settings_rules_v17, settings_stock_v5, takvin_v5, views
 
 urlpatterns = [
     path("", excel_dashboard.dashboard, name="dashboard"),
@@ -9,6 +9,7 @@ urlpatterns = [
     path("sales/select/<int:jy>/<int:jm>/<int:jd>/", daily_views.select_sale_day, name="select_sale_day"),
     path("sales/<int:day_id>/", sale_brand_v19.sale_brand, name="sale_brand"),
     path("sales/<int:day_id>/report/", daily_report_v8.daily_report, name="daily_report"),
+    path("sales/<int:day_id>/return/", daily_returns_v36.daily_return_add, name="daily_return_add"),
     path("sales/<int:day_id>/import-xlsx/", daily_order_views_v8.import_daily_orders, name="daily_order_import"),
     path("sales/<int:day_id>/<int:brand_id>/<int:size_id>/", daily_views.sale_size, name="sale_size"),
     path("sales/save/", excel_sales.sale_line_save, name="sale_line_save"),
