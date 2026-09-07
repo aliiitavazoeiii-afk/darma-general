62e21aad7e17bdedd4657dce3fa171ace516b63a
aliiitavazoeiii-afk <aliiitavazoeiii@gmail.com>
2026-09-07 20:11:14 +0330

    build expense tracker v1 foundation

---
+++ expense_site/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
+
+urlpatterns = [
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="expense_tracker/login.html"),
+        name="expense_login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="expense_logout"),
+    path("", include("expense_tracker.urls")),
+]
