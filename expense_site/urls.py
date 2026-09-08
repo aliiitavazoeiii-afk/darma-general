239f9de51eb7969323f3199d397fbb3303941582
aliiitavazoeiii-afk <aliiitavazoeiii@gmail.com>
2026-09-08 20:18:49 +0330

    wire expense PWA endpoints

--- expense_site/urls.py
+++ expense_site/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from core import calendar_views
+from expense_tracker import pwa
 
 urlpatterns = [
     path(
@@ -10,6 +11,8 @@ urlpatterns = [
         name="expense_login",
     ),
     path("logout/", auth_views.LogoutView.as_view(), name="expense_logout"),
+    path("manifest.webmanifest", pwa.manifest, name="expense_manifest"),
+    path("sw.js", pwa.service_worker, name="expense_service_worker"),
     path("calendar/picker/", calendar_views.jalali_picker, name="expense_calendar_picker"),
     path("", include("expense_tracker.urls")),
 ]
