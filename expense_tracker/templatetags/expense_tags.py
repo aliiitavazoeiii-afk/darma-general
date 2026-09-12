f92d1e785a424028d2e93195333d419d89d561b0
aliiitavazoeiii-afk <aliiitavazoeiii@gmail.com>
2026-09-12 06:34:20 +0330

    add elapsed-month daily expense average filter

---
+++ expense_tracker/templatetags/expense_tags.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def daily_average(month_total, jalali_date):
+    try:
+        total = int(month_total or 0)
+        day = int(str(jalali_date or "").split("/")[-1])
+        day = max(1, day)
+        return int(round(total / day))
+    except (TypeError, ValueError):
+        return 0
